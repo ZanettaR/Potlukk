a3527bcfd77de9fab83dabd4669867d264298798
qpco <qpco@pm.me>
2022-05-11 09:36:57 -0500

    replace: method implementation

--- src/main/java/dev/group1/Potlukk/entities/Potlukk.java
+++ src/main/java/dev/group1/Potlukk/entities/Potlukk.java
@@ -7,11 +7,13 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "potluck")
 public class Potlukk {
     @Id
     @Column(name = "p_id")
