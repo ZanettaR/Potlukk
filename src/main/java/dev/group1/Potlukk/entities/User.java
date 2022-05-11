692f43b8e85f1e3e0dbf54f73fc8a7b16a870596
Zanetta Tyler <zrtyler@aggies.ncat.edu>
2022-05-11 13:01:16 -0400

    Create User Inteface

--- src/main/java/dev/group1/Potlukk/entities/User.java
+++ src/main/java/dev/group1/Potlukk/entities/User.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
@@ -16,6 +13,7 @@ import javax.persistence.Table;
 @Table(name = "customer")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private int id;
     @Column(name = "username")
